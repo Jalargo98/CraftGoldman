0eaf90da4d49b7393d38a94d10432a44e59768cd
Jalar98 <jalargo60@misena.edu.co>
2024-04-04 01:59:04 -0500

    Se realizo el crud de subcategoria

--- admin/views.py
+++ admin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from Productos.models import Categoria
+from Productos.models import Categoria,Subcategoria
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
@@ -41,3 +41,45 @@ def categoriaU(request,id_categoria):
   else:
     context = {'categoria':cat}
     return render(request,'admin/categoriaC.html',context)
+  
+  
+
+def subcategoriaC(request):
+  if request.method == 'POST':
+    try:
+      cat = Subcategoria()
+      cat.nombre = request.POST.get('nombre')
+      cat.categoria = Categoria.objects.get(id=request.POST.get('categoria'))
+      print(cat)
+      cat.save()
+      return redirect('admin:subcategoriaR')
+    except KeyError as e:
+      print(e)
+      return redirect('admin:subcategoriaC')
+    except Exception as e:
+      print(e)
+      return redirect('admin:subcategoriaC')
+  else:
+    cat = Categoria.objects.all()
+    context = {'categorias': cat}
+    return render(request,'admin/subcategoriaC.html',context)
+
+def subcategoriaR(request):
+  cat = Subcategoria.objects.all()
+  context = {'subcategorias':cat}
+  return render(request,'admin/subcategoriaR.html',context)
+
+def subcategoriaU(request,id_subcategoria):
+  try:
+    subcat = Subcategoria.objects.get(id=id_subcategoria)
+    cat = Categoria.objects.all()
+  except ObjectDoesNotExist:
+    return redirect('admin:subcategoriaR')
+  if request.method == "POST":
+    subcat.nombre = request.POST.get('nombre')
+    subcat.categoria = Categoria.objects.get(id=request.POST.get('categoria'))
+    subcat.save()
+    return redirect('admin:subcategoriaR')
+  else:
+    context = {'categorias':cat,'subcategoria':subcat}
+    return render(request,'admin/subcategoriaC.html',context)
